189c4854cf6dc45f461a690a8041767b45cc965a
boubacarsow <42849861+boubacarsow@users.noreply.github.com>
2022-10-19 12:08:46 -0600

    updates at 12:08 on 10/19/2022

--- objects.js
+++ objects.js
@@ -29,15 +29,70 @@
 
 // login(userOneEmail);
 
-var userOne = {
-    email: 'ryu@ninjas.com',
-    name: 'Ryu',
-    login(){
-        console.log(this.email, 'has logged in');
-    },
-    logout(){
-        console.log(this.email, 'has logged out');
-    }
-};
+// var userOne = {
+//     email: 'ryu@ninjas.com',
+//     name: 'Ryu',
+//     login(){
+//         console.log(this.email, 'has logged in');
+//     },
+//     logout(){
+//         console.log(this.email, 'has logged out');
+//     }
+// };
 
-console.log(userOne.name);
+// userOne.name = 'Yoshi';
+
+// var userOne = {
+//     email: 'ryu@ninjas.com',
+//     name: 'Ryu',
+//     login(){
+//         console.log(this.email, 'has logged in');
+//     },
+//     logout(){
+//         console.log(this.email, 'has logged out');
+//     }
+// };
+
+// userOne.name = 'Yoshi';
+
+// var userTwo = {
+//     email: 'ryu@ninjas.com',
+//     name: 'Ryu',
+//     login(){
+//         console.log(this.email, 'has logged in');
+//     },
+//     logout(){
+//         console.log(this.email, 'has logged out');
+//     }
+// };
+
+// userTwo.name = 'Yoshi';
+
+// var userThree = {
+//     email: 'ryu@ninjas.com',
+//     name: 'Ryu',
+//     login(){
+//         console.log(this.email, 'has logged in');
+//     },
+//     logout(){
+//         console.log(this.email, 'has logged out');
+//     }
+// };
+
+// userThree.name = 'Yoshi';
+
+// var userFour = new user()
+
+class User {
+    constructor(email, name){
+        this.email = email;
+        this.name = name;
+
+    }
+}
+
+var userOne = new User('ryu@ninjas.com', 'Ryu');
+var userTwo = new User('yoshi@mariokorp.com', 'Yoshi');
+
+console.log(userOne);
+console.log(userTwo);
